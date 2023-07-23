d38d891ee47a8d162ba58f63ace0f1e144bfdac7
khaled <khaled.abdelhady99@gmail.com>
2023-07-23 11:41:56 +0300

    version 1 restored

--- lcd/main.c
+++ lcd/main.c
@@ -34,16 +34,16 @@ void LCD_Command( unsigned char cmnd )
 	LCD_Port = (LCD_Port & 0x0F) | (cmnd & 0xF0); /* sending upper nibble */
 	LCD_Port &= ~ (1<<RS);				/* RS=0, command reg. */
 	LCD_Port |= (1<<EN);				/* Enable pulse */
-	_delay_us(5);
+	_delay_us(1);
 	LCD_Port &= ~ (1<<EN);
 
 	_delay_us(200);
 
 	LCD_Port = (LCD_Port & 0x0F) | (cmnd << 4);  /* sending lower nibble */
 	LCD_Port |= (1<<EN);
-	_delay_us(3);
+	_delay_us(1);
 	LCD_Port &= ~ (1<<EN);
-	_delay_ms(4);
+	_delay_ms(2);
 }
 
 
